3cfe9c76e592ebfed0fb2c2e129fa60edfcdecb4
Андрей Барбашин <andrejbarbasin@MacBook-Pro-gmate.local>
2019-11-28 14:25:00 +0300

    minor

--- web/app/themes/theme_wp/js/script.js
+++ web/app/themes/theme_wp/js/script.js
@@ -2,9 +2,9 @@ var swiper = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     spaceBetween: 20,
     loop: true,
-    // autoplay: {
-    //     delay: 10000,
-    // },
+    autoplay: {
+        delay: 10000,
+    },
     navigation: {
         nextEl: '.bill__swipe-next',
         prevEl: '.bill__swipe-prev',
