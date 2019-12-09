576e86437dcd26368e6d4ba300bd7a60f4c69d7a
Андрей Барбашин <andrejbarbasin@MacBook-Pro-gmate.local>
2019-12-09 12:59:57 +0300

    Footer Nonna

--- web/app/themes/theme_wp/footer.php
+++ web/app/themes/theme_wp/footer.php
@@ -24,19 +24,26 @@
             <?php get_template_part('/template-parts/footer/footer', '4') ?>
         </div>
         <div class="footer__bottom">
-            <div class="footer__copy">
-                © 2019, ИП Гарипова Нонна Камоевна, ИНН: 165608813105, ОРГН: 310169004700047
-            </div>
-            <div class="footer__bottomlist">
-                <div class="bottomlist__item">
-                    <a href="#">Статистика</a>
+            <p>Обращаем внимание, что сайт "gdenoski.ru" по продаже носков, колготок и чулок носит исключительно информационный характер и ни при каких условиях не является публичной офертой, определяемой положениями Статьи 437 (2) ГК РФ.</p>
+            <p>gdenoski.ru использует файлы «cookie», с целью персонализации сервисов и повышения удобства пользования веб-сайтом. «Cookie» представляют собой небольшие файлы, содержащие информацию о предыдущих посещениях веб-сайта. Если вы не хотите использовать файлы «cookie», измените настройки браузера.</p>
+        </div>
+    </div>
+    <div class="footer__info">
+        <div class="container">
+            <div class="row">
+                <div class="footer__copy">
+                    © 2019, ИП Гарипова Нонна Камоевна, ИНН: 165608813105, ОРГН: 310169004700047
                 </div>
-                <div class="bottomlist__item">
-                    <a href="#">Реклама</a>
+                <div class="footer__bottomlist">
+                    <div class="bottomlist__item">
+                        <a href="#">Статистика</a>
+                    </div>
+                    <div class="bottomlist__item">
+                        <a href="#">Реклама</a>
+                    </div>
                 </div>
             </div>
         </div>
-<!--        <p>SQL queries: --><?//= get_num_queries() ?><!--</p>-->
     </div>
 </footer>
 </body>
